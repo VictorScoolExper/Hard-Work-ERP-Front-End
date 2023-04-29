a4c34b966f5dc4b5c56ec39402ddc936e3c676e0
VictorScoolExper <victorscool477@outlook.com>
2023-04-28 18:18:48 -0600

    completed check user logged in

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,13 +1,38 @@
 import { redirect } from "react-router-dom";
+import axios from "axios";
+import { AUTH_API_URL } from "../api-routes";
 
-function checkLocalStorage(key){
+function checkLocalStorage(key) {
   return localStorage.getItem(key) !== null;
 }
-  
-export function checkUserLogged() {
-  if(checkLocalStorage('user')){
+
+const checkUserPermission = async() =>{
+  try {
+    const response = await axios(AUTH_API_URL + "/checkPermission", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true
+    });
+
+    if(response.data.valid === 'true'){
+      return true;
+    } 
+    return false;
+    
+  } catch (error) {
+    return false;
+  }
+    
+}
+
+export async function checkUserLogged() {
+  const permission = await checkUserPermission();
+  console.log(permission);
+  if (checkLocalStorage("user") && permission) {
     return null;
-  } 
+  }
+  localStorage.clear();
   return redirect("/auth");
 }
-  
